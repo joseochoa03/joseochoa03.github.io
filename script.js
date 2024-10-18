364e17275fd93fb4e2711c3d2a43991a200ac549
joseochoa03 <sabiduriadeloalto03@gmail.com>
2024-10-18 13:06:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -95,11 +95,3 @@ window.onload = () => {
   playVideo("Videos Recientes", 0);
   updateVisitCount();
 };
-
-function closeForm() {
-    document.getElementById("welcomeModal").style.display = "none";
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("welcomeModal").style.display = "block";
-});
